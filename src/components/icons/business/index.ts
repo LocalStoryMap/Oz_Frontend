a306428748c011e17aadc607ff8e2765d03dbda8
Leekwanyong <dlrkdid12@gmail.com>
2025-05-28 22:28:02 +0900

    🔧 refactor: #12 아이콘 export 이름 정리

--- src/components/icons/business/index.ts
+++ src/components/icons/business/index.ts
@@ -1,4 +1,4 @@
-export { default as UserIcon } from './CustomerIcon';
 export { default as JourneyIcon } from './JourneyIcon';
 export { default as BookIcon } from './OpenBookIcon';
+export { default as UserIcon } from './UserIcon';
 export { default as VersionIcon } from './VersionIcon';
