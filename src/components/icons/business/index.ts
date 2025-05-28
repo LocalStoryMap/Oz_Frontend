4bfda7deba9fed5638fc0e7817c24a4d0efab164
Leekwanyong <dlrkdid12@gmail.com>
2025-05-28 20:09:18 +0900

    :sparkles: feat: #12 아이콘 일괄 export 구조 구현

---
+++ src/components/icons/business/index.ts
@@ -0,0 +1,4 @@
+export { default as UserIcon } from './CustomerIcon';
+export { default as JourneyIcon } from './JourneyIcon';
+export { default as BookIcon } from './OpenBookIcon';
+export { default as VersionIcon } from './VersionIcon';
---
+++ src/components/icons/location/index.ts
@@ -0,0 +1,4 @@
+export { default as GpsLocationIcon } from './GpsLocationIcon';
+export { default as LocationIcon } from './LocationIcon';
+export { default as MapLocationIcon } from './MapLocationIcon';
+export { default as MapMarkerIcon } from './MapMarkerIcon';
