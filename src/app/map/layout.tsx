65d192da9a4bf8f6a015f8926b33c944e29fed77
seonysun <csh99240@naver.com>
2025-06-05 13:17:23 +0900

    ♻️ refactor: #34 search 컴포넌트 적용

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -1,6 +1,7 @@
 import { GpsLocationIcon } from '@/components/icons';
 import { iconWrapper } from '@/components/map/map.recipe';
 import PageHeader from '@/components/ui/common/pageHeader/PageHeader';
+import { Search } from '@/components/ui/common/textfield';
 
 import { css } from '@root/styled-system/css';
 
@@ -8,14 +9,23 @@ function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <PageHeader>
-        <div className={css({ flex: 1, marginLeft: 10, textAlign: 'center' })}>
-          서치바
+        <div
+          className={css({
+            flex: 1,
+            marginLeft: 10,
+            paddingX: 3,
+            textAlign: 'center',
+          })}
+        >
+          <Search placeholder="Search" />
         </div>
         <div className={iconWrapper()}>
           <GpsLocationIcon fill="#707070" />
         </div>
       </PageHeader>
-      <div className={css({ width: 'full', overflow: 'auto' })}>{children}</div>
+      <div className={css({ width: 'full', overflow: 'auto', marginTop: 3 })}>
+        {children}
+      </div>
     </>
   );
 }
