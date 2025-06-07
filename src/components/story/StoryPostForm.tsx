89b8bc431194950890e8aad0b3fc5fe1a6e5f4d2
seonysun <csh99240@naver.com>
2025-06-07 19:13:51 +0900

    ✨ feat: #51 스토리 작성 페이지 구현

---
+++ src/components/story/StoryPostForm.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Button } from '@/components/ui/common/buttons/Button';
+import { flex } from '@/components/ui/common/cards/card.recipe';
+import { modalText } from '@/components/ui/common/modals/modal.recipe';
+import { Search, Textarea } from '@/components/ui/common/textfields';
+
+function StoryPostForm() {
+  return (
+    <div className={flex({ gap: 'xl' })}>
+      <div className={flex({ gap: 'lg', p: 'sm', marginB: 'sm' })}>
+        <p className={modalText({ text: 'body2', align: 'left' })}>글작성</p>
+        <Search radius="md" placeholder="위치를 지정해주세요" />
+        <Textarea
+          size="lg"
+          radius="md"
+          placeholder="직접 다녀온 생생한 후기를 작성해보세요"
+          fullWidth
+          style={{ minHeight: '150px', fontWeight: 400 }}
+        />
+        <Button color="outline" size="sm">
+          이미지/동영상 첨부
+        </Button>
+      </div>
+      <Button color="black">작성 완료</Button>
+    </div>
+  );
+}
+
+export default StoryPostForm;
