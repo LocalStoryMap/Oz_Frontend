29bab04d22ae65365e410ba3b9d518b246f7cc17
seonysun <csh99240@naver.com>
2025-06-07 12:18:54 +0900

    🐛 fix: #34 useSearchParams 클라이언트 훅 오류 해결을 위한 suspense 적용

--- src/app/map/search/page.tsx
+++ src/app/map/search/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from 'react';
+
 import MapSearch from '@/components/map/MapSearch';
 
 function page() {
-  return <MapSearch />;
+  return (
+    <Suspense fallback={<div>로딩 중</div>}>
+      <MapSearch />
+    </Suspense>
+  );
 }
 
 export default page;
