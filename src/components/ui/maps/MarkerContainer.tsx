01f73ad9e3410255c97593cd8f0e312f0bc66973
seonysun <csh99240@naver.com>
2025-06-10 00:32:47 +0900

    ✨ feat: #54 마커 컴포넌트에 이미지 적용을 위한 type 속성 추가

--- src/components/ui/maps/MarkerContainer.tsx
+++ src/components/ui/maps/MarkerContainer.tsx
@@ -3,19 +3,31 @@
 import { useState } from 'react';
 import { MapMarker, MapMarkerProps, useMap } from 'react-kakao-maps-sdk';
 
+import { MAP_CATEGORY } from '@/constants/map';
+
 type MarkerContainerProps = {
   position: MapMarkerProps['position'];
+  type?: (typeof MAP_CATEGORY)[number]['value'];
   content?: string;
   onClick?: () => void;
 };
 
-function MarkerContainer({ position, content, onClick }: MarkerContainerProps) {
+function MarkerContainer({
+  position,
+  type,
+  content,
+  onClick,
+}: MarkerContainerProps) {
   const map = useMap();
   const [isVisible, setIsVisible] = useState(false);
 
   return (
     <MapMarker
       position={position}
+      image={{
+        src: `/images/markers/${type}.png`,
+        size: { width: 32, height: 32 },
+      }}
       clickable
       onMouseOver={() => setIsVisible(true)}
       onMouseOut={() => setIsVisible(false)}
