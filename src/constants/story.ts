73512e6e9d4075c2a83a984673ffbeb54299c53f
seonysun <csh99240@naver.com>
2025-06-09 11:50:24 +0900

    ✨ feat: #51 스토리 작성 페이지 감정 아이콘 선택 필드 추가

---
+++ src/constants/story.ts
@@ -0,0 +1,15 @@
+import {
+  Angry,
+  Frowning,
+  Grinning,
+  Neutral,
+  Smiling,
+} from '@/components/icons/feelings';
+
+export const feelings = [
+  { label: Smiling, value: 'smile' },
+  { label: Grinning, value: 'grinning' },
+  { label: Neutral, value: 'neutral' },
+  { label: Frowning, value: 'frown' },
+  { label: Angry, value: 'angry' },
+];
