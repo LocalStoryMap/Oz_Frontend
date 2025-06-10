c42c9212b400397c9a763ae522d8995dd168d929
seonysun <csh99240@naver.com>
2025-06-10 09:50:10 +0900

    💄 design: #65 스토리 페이지 내부 헤더 추가

--- src/app/story/search/page.tsx
+++ src/app/story/search/page.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from 'react';
 
 import StoryPlaceList from '@/components/story/StoryPlaceList';
+import PageHeader from '@/components/ui/common/pageHeader/PageHeader';
 
 function page() {
   return (
-    <Suspense fallback={<div>로딩 중</div>}>
-      <StoryPlaceList />
-    </Suspense>
+    <>
+      <PageHeader title="스토리" />
+      <Suspense fallback={<div>로딩 중</div>}>
+        <StoryPlaceList />
+      </Suspense>
+    </>
   );
 }
 
