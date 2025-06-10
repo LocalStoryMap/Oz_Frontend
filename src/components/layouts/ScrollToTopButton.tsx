6987bf03ae2903fd6953a5ec4a59d063ef9a6613
Leekwanyong <dlrkdid12@gmail.com>
2025-06-10 12:52:22 +0900

    :sparkles: feat: #66 ScrollToTopButton구현

---
+++ src/components/layouts/ScrollToTopButton.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+import { css } from '@root/styled-system/css';
+
+function ScrollToTopButton() {
+  const [showButton, setShowButton] = useState<boolean>(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    const handleShowButton = () => {
+      if (window.scrollY > 500 && !showButton) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    window.addEventListener('scroll', handleShowButton);
+    return () => {
+      window.removeEventListener('scroll', handleShowButton);
+    };
+  }, [showButton]);
+  return (
+    showButton && (
+      <div className={css({ position: 'relative' })}>
+        <button
+          type="button"
+          className={css({
+            position: 'fixed',
+            p: 4,
+            bg: 'black',
+            color: 'white',
+            borderRadius: 'full',
+            zIndex: 10,
+            transition: 'all 0.3s ease-in-out',
+            cursor: 'pointer',
+            top: '82%',
+            right: '4rem',
+          })}
+          onClick={scrollToTop}
+        >
+          Top
+        </button>
+      </div>
+    )
+  );
+}
+
+export default ScrollToTopButton;
