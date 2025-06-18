ca72b9ba5f495f1d0aef37a0763bd7fbdbed142b
Leekwanyong <dlrkdid12@gmail.com>
2025-06-18 17:23:19 +0900

    :bug: fix: #106 배포 오류 수정

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'kr.object.ncloudstorage.com',
+      'img1.kakaocdn.net',
+      't1.kakaocdn.net',
+    ],
+  },
+};
+
+module.exports = nextConfig;
