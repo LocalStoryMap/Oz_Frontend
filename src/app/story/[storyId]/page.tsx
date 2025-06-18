6ffd73199872da99ae02a1b6d0e90b24a80a6e3b
Leekwanyong <dlrkdid12@gmail.com>
2025-06-18 23:03:31 +0900

    :sparkles: feat: #111 게시물 상세 조회 api 연결

--- src/app/story/[storyId]/page.tsx
+++ src/app/story/[storyId]/page.tsx
@@ -1,17 +1,26 @@
+'use client';
+
 import React from 'react';
+import { ENDPOINTS } from '@api/endpoints';
+import { instance } from '@api/instance';
 import StoryDetailCommentSection from '@components/story/detail/sections/StoryDetailCommentSection';
 import StoryDetailContent from '@components/story/detail/sections/StoryDetailContent';
-
-import { mockStoryList } from '@/mocks/mockStoryList';
+import { useQuery } from '@tanstack/react-query';
 
 function Page({ params }: { params: { storyId: string } }) {
-  const findData = mockStoryList.find(
-    item => item.id === Number(params.storyId),
-  );
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['storyDetail', params.storyId],
+    queryFn: () => instance.get(ENDPOINTS.STORY.DETAIL(params.storyId)),
+  });
+
+  if (isLoading) return <div>로딩중...</div>;
+  if (isError) return <div>Error...</div>;
+
+  console.log(data?.data);
   return (
     <section>
-      <StoryDetailContent findData={findData} />
-      <StoryDetailCommentSection />
+      <StoryDetailContent story={data?.data} />
+      <StoryDetailCommentSection story={data?.data} />
     </section>
   );
 }
