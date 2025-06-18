702b19b7b5304ee04721b67e8d20337f0cb1e642
seonysun <csh99240@naver.com>
2025-06-18 15:01:39 +0900

    🐛 fix: #102 api 응답값 형식에 맞춰 queryFn 수정

--- src/api/options/markerOption.ts
+++ src/api/options/markerOption.ts
@@ -4,11 +4,25 @@ import { ENDPOINTS } from '@/api/endpoints';
 import { mutationFetcher, queryFetcher } from '@/api/fetcher';
 import { Marker } from '@/types/marker';
 
+type MarkerListResponse = {
+  data: Marker[];
+  pagination: {
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+    itemsPerPage: number;
+  };
+};
+
 export const markerOption = {
   getMarkerList: (params?: Record<string, any>) =>
-    queryOptions<Marker[]>({
+    queryOptions<MarkerListResponse>({
       queryKey: ['marker', 'list', params],
-      queryFn: queryFetcher(ENDPOINTS.MARKER.LIST, params),
+      queryFn: () =>
+        queryFetcher(ENDPOINTS.MARKER.LIST, params)().then(res => ({
+          data: res.data,
+          pagination: res.pagination,
+        })),
     }),
   postMarker: () => ({
     mutationFn: (data: any) =>
