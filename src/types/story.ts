3d591175a9f1c808d9577ad678326014668d99c2
seonysun <csh99240@naver.com>
2025-06-20 09:30:55 +0900

    ✨ feat: #115 스토리 타입 정의

--- src/types/story.ts
+++ src/types/story.ts
@@ -7,3 +7,18 @@ export type PostStoryPayload = {
   };
   images: File[];
 };
+
+export type Story = {
+  storyId: number;
+  userNickname: string;
+  userProfileImage: string;
+  marker: number;
+  title: string;
+  content: string;
+  emoji: string;
+  viewCount: number;
+  likeCount: number;
+  createdAt: string;
+  updatedAt: string;
+  isLiked: boolean;
+};
