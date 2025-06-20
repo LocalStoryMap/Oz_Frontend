90385197ef6e4c4ee0f21f8c9883ddad3d172e21
Leekwanyong <dlrkdid12@gmail.com>
2025-06-21 05:15:53 +0900

    :recycle: refactor: #131 story.types.ts type 추가

--- src/types/story.types.ts
+++ src/types/story.types.ts
@@ -1,15 +1,26 @@
+export type StoryType = {
+  createdAt: string;
+  title: string;
+  content: string;
+  userNickname: string;
+  userProfileImage: string;
+  isLiked: boolean;
+  likeCount: number;
+  viewCount: number;
+};
+
 export type StoryCardProps = {
   story: {
     storyId: string;
     images?: string[];
     userProfile?: string;
-    title?: string;
-    content?: string;
+    title: string;
+    content: string;
     likeCount?: number;
     viewCount?: number;
     isLiked?: boolean;
-    userNickname?: string;
-    createdAt?: string;
+    userNickname: string;
+    createdAt: string;
   };
 };
 
