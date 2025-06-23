ad9393db156594906f64ada48e96c07578219662
Leekwanyong <dlrkdid12@gmail.com>
2025-06-24 01:33:13 +0900

    :bug: fix: #152 ci/cd 오류 수정

--- src/components/story/detail/StoryContentActions.tsx
+++ src/components/story/detail/StoryContentActions.tsx
@@ -1,10 +1,13 @@
 'use client';
 
-import React from 'react';
+import { useParams, useRouter } from 'next/navigation';
 import { Button } from '@components/ui/common/buttons/Button';
 import { useMutation } from '@tanstack/react-query';
 
 import { followsOption } from '@/api/options/followsOption';
+import { storyOption } from '@/api/options/storyOption';
+import Modal from '@/components/ui/common/modals/Modal';
+import { useModalStore } from '@/store/useModalStore';
 
 import { css } from '@root/styled-system/css';
 
@@ -15,61 +18,95 @@ type Props = {
 };
 
 function StoryContentActions({ mode, isMine, userNickname }: Props) {
-  const mutation = useMutation({
+  const router = useRouter();
+  const params = useParams();
+  const id = params?.storyId as string;
+
+  const followMutation = useMutation({
     ...followsOption.postFollows(),
   });
 
-  const onSubmit = () => {
-    mutation.mutate({ nickname: userNickname });
+  const handleFollow = () => {
+    followMutation.mutate({ nickname: userNickname });
   };
 
+  const deleteMutation = useMutation({
+    ...storyOption.deleteStory(id),
+    onSuccess: () => {
+      router.push('/story');
+    },
+  });
+
+  const openModal = useModalStore(state => state.open);
+  const isModalOpen = useModalStore(state => state.isOpen);
+
   return (
-    <div
-      className={css({
-        display: 'flex',
-        alignItems: 'center',
-        gap: 4,
-        ml: 'auto',
-      })}
-    >
-      <Button
-        size="sm"
-        color="outlineSoft"
-        aria-label="팔로우 버튼"
-        onClick={onSubmit}
+    <>
+      <div
+        className={css({
+          display: 'flex',
+          alignItems: 'center',
+          gap: 4,
+          ml: 'auto',
+        })}
       >
-        {mutation.isPending ? 'asd' : 'Follows'}
-      </Button>
-      {mode === 'story' && isMine && (
-        <>
-          <Button
-            size="sm"
-            color="custom"
-            className={css({
-              _hover: {
-                bg: 'blue.50',
-              },
-            })}
-            aria-label="글 수정 버튼"
-          >
-            수정
-          </Button>
-          <Button
-            size="sm"
-            color="custom"
-            className={css({
-              _hover: {
-                bg: 'blue.50',
-                color: 'red',
-              },
-            })}
-            aria-label="글 삭제 버튼"
-          >
-            삭제
-          </Button>
-        </>
+        {/* 팔로우 버튼 */}
+        <Button
+          size="sm"
+          color="outlineSoft"
+          aria-label="팔로우 버튼"
+          onClick={handleFollow}
+        >
+          {followMutation.isPending ? '로딩중...' : 'Follows'}
+        </Button>
+        {/* 수정/삭제 버튼 */}
+        {mode === 'story' && isMine && (
+          <>
+            <Button
+              size="sm"
+              color="custom"
+              className={css({
+                _hover: {
+                  bg: 'blue.50',
+                },
+              })}
+              aria-label="글 수정 버튼"
+              onClick={() => router.push(`/story/post/${id}`)}
+            >
+              수정
+            </Button>
+            <Button
+              size="sm"
+              color="custom"
+              className={css({
+                _hover: {
+                  bg: 'blue.50',
+                  color: 'red',
+                },
+              })}
+              aria-label="글 삭제 버튼"
+              onClick={openModal}
+              disabled={deleteMutation.isPending}
+            >
+              삭제
+            </Button>
+          </>
+        )}
+      </div>
+      {isModalOpen && (
+        <Modal
+          title="정말 삭제하시겠어요?"
+          content={
+            <>
+              삭제 시 모든 정보가 삭제되며,
+              <br />
+              복구는 불가능합니다.
+            </>
+          }
+          onConfirm={() => deleteMutation.mutate()}
+        />
       )}
-    </div>
+    </>
   );
 }
 
