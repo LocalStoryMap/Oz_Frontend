9a4dbd43cbcd84d10928e0cae66454696e77dc2d
Leekwanyong <dlrkdid12@gmail.com>
2025-06-24 00:35:57 +0900

    :bug: fix: #149 ci/cd 오류 수정

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import UserPageContent from '@/app/user/UserPageContent';
+import UserPageContent from '@/app/user/_components/UserPageContent';
 
 export const dynamic = 'force-dynamic';
 
