4a368453b4f308cda5c1a10762187372538e2d0f
seonysun <csh99240@naver.com>
2025-06-25 15:14:11 +0900

    ✨ feat: #97 모달 전역 스토어에 모달 타입, 데이터 props 추가

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,13 +1,19 @@
 import { create } from 'zustand';
 
+type ModalType = 'confirm' | 'content' | null;
+
 type ModalState = {
+  type?: ModalType;
   isOpen: boolean;
-  open: () => void;
+  data?: any;
+  open: (type?: ModalType, data?: any) => void;
   close: () => void;
 };
 
 export const useModalStore = create<ModalState>(set => ({
+  type: null,
   isOpen: false,
-  open: () => set({ isOpen: true }),
-  close: () => set({ isOpen: false }),
+  data: undefined,
+  open: (type, data) => set({ isOpen: true, type, data }),
+  close: () => set({ isOpen: false, type: null, data: undefined }),
 }));
