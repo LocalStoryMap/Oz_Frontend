fce0cfae65f84cfb42dac9d472d821086e06f81a
Leekwanyong <dlrkdid12@gmail.com>
2025-06-26 08:30:29 +0900

    :sparkles: feat: #190 구독 type 정의

---
+++ src/types/subscribeId.type.ts
@@ -0,0 +1,11 @@
+export type Subscribe = {
+  subscribeId: number;
+  user: number;
+  userEmail: string;
+  impUid: string;
+  merchantUid: string;
+  isActive: boolean;
+  startAt: string;
+  expiresAt: string;
+  isPaidUser: boolean;
+};
