f647eb965785024e1e7fc824f90db244fa1c92d9
seonysun <csh99240@naver.com>
2025-06-25 22:44:25 +0900

    ✨ feat: #97 전역 modal 스토어에 id props 추가

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -3,17 +3,19 @@ import { create } from 'zustand';
 type ModalType = 'confirm' | 'content' | null;
 
 type ModalState = {
+  id?: number;
   type?: ModalType;
   isOpen: boolean;
   data?: any;
-  open: (type?: ModalType, data?: any) => void;
+  open: (type?: ModalType, data?: any, id?: number) => void;
   close: () => void;
 };
 
 export const useModalStore = create<ModalState>(set => ({
+  id: 0,
   type: null,
   isOpen: false,
   data: undefined,
-  open: (type, data) => set({ isOpen: true, type, data }),
-  close: () => set({ isOpen: false, type: null, data: undefined }),
+  open: (type, data, id) => set({ isOpen: true, type, data, id }),
+  close: () => set({ isOpen: false, type: null, data: undefined, id: 0 }),
 }));
