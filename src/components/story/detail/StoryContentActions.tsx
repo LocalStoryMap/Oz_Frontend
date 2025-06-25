12d6ebb9121a52fbb78279653ed6af7a62f47917
seonysun <csh99240@naver.com>
2025-06-25 15:25:13 +0900

    🐛 fix: #97 스토리 상세 삭제 확인 시 모달 close 이벤트 추가

--- src/components/story/detail/StoryContentActions.tsx
+++ src/components/story/detail/StoryContentActions.tsx
@@ -49,12 +49,16 @@ function StoryContentActions({ mode, isMine, userNickname }: Props) {
   const deleteMutation = useMutation({
     ...storyOption.deleteStory(id),
     onSuccess: () => {
+      close();
       router.push('/story');
     },
   });
 
-  const openModal = useModalStore(state => state.open);
-  const isModalOpen = useModalStore(state => state.isOpen);
+  const { open, isOpen, close } = useModalStore(state => ({
+    open: state.open,
+    isOpen: state.isOpen,
+    close: state.close,
+  }));
 
   return (
     <>
@@ -100,14 +104,14 @@ function StoryContentActions({ mode, isMine, userNickname }: Props) {
                 },
               })}
               aria-label="글 삭제 버튼"
-              onClick={openModal}
+              onClick={() => open()}
             >
               삭제
             </Button>
           </>
         )}
       </div>
-      {isModalOpen && (
+      {isOpen && (
         <Modal
           title="정말 삭제하시겠어요?"
           content={
@@ -117,7 +121,9 @@ function StoryContentActions({ mode, isMine, userNickname }: Props) {
               복구는 불가능합니다.
             </>
           }
-          onConfirm={() => deleteMutation.mutate()}
+          onConfirm={() => {
+            deleteMutation.mutate();
+          }}
         />
       )}
     </>
